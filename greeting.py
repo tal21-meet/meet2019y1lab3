fffbf61201d123990d951fa2a6c806864b831ebc
tal21-meet <tal21@meet.mit.edu>
2019-07-23 07:33:35 -0400

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,10 @@
+name=input("what's your name?")
+name2=name.title()
+print("hello there, "+name2)
+name_num=len(name)
+name_num2=str(name_num)
+print ("Your name is "+ name_num2 +" letters long!")
+first_lett=name[0]
+leset_lett=name[-1]
+print("The first letter of your name is "+first_lett+' and the last letter is '+leset_lett)
+print(name[1:-1])
